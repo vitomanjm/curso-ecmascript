1998aac1401d858f9ae72c8822d118c1f8d1d0e0
vitomanjm <vitomanjm@gmail.com>
2023-02-06 12:57:26 -0500

    First class ECMAScript course

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,27 @@
+
+ var lastName = 'David';
+ lastName = 'Ricardo';
+ console.log(lastName);
+
+
+let fruit = 'Apple';
+fruit = 'Kiwi';
+
+const animal = 'Cat';
+animal = 'Dog';
+console.log(animal);
+
+const fruits = () => {
+    if(true){
+        var fruit1 = 'Apple';
+        let fruit2 = 'Berry';
+        const fruit3 = 'Mango';
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+    }
+
+    fruits();
+
+
---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,11 @@
+function square(num) {
+    return num * num;
+}
+
+square();
+
+const square = (num) => {
+    return num*num;
+}
+
+const square = num => num*num;
